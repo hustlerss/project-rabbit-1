2d72334b5164e06c865a1990700471be7d9fb363
Rohan Kirdak <your-email@gmail.com>
2026-04-19 22:23:59 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-var express = require("express");
+const express = require("express");
 var bodyparser = require("body-parser");
 var upload = require("express-fileupload");
 var session = require("express-session");
@@ -7,7 +7,7 @@ var path = require("path");
 var user_route = require("./routes/user_routes");
 var admin_route = require("./routes/admin_routes");
 
-var app = express();
+const app = express();
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
